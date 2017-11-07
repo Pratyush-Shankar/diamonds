3ddbd0147115a757a0e541f7e2041fea1832fe0f
Pratyush-Shankar <31359064+Pratyush-Shankar@users.noreply.github.com>
2017-11-06 19:57:07 -0800

    Created my first Shiny App

---
+++ diamondsVis/app.R
@@ -0,0 +1,33 @@
+library(shiny)
+library(ggplot2)
+
+#GUI
+ui = fluidPage(
+  titlePanel("Diamonds  Data"),
+  sidebarLayout(
+    sidebarPanel(helpText("This is app to visualize diamonds dataset"),
+                 textInput(inputId = "title",
+                           label = "Chart title"),
+                 value = ""),
+    selectInput(inputId = "pos",
+                label = "Position",
+                choices = list("stack","dodge"),
+                selected = "stack")),
+      mainPanel(
+        plotOutput(outputId = "plot")
+      )
+    )
+
+
+#Server
+server = function(input, output){
+        
+  output$plot = renderPlot({
+          ggplot(diamonds, aes(x=cut, fill= clarity)) +
+            geom_bar(position = input$pos) +
+      ggtitle(input$title)
+        })  
+}
+
+#Run the App
+shinyApp(ui,server)
